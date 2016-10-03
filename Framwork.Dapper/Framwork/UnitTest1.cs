d13074298d00113926631040b3c6686d84e4158e
fred <fredli.ex@gmail.com>
2016-10-04 00:08:10 +0800

    init

---
+++ Framwork.Dapper/Framwork/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Framwork
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

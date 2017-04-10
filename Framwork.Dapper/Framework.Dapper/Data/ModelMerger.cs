8d995c9cf64d37e8924a92d320989978be1ddc54
fredli.ex <fredli.ex@gmail.com>
2017-04-10 18:06:24 +0800

    aa

---
+++ Framwork.Dapper/Framework.Dapper/Data/ModelMerger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Framework.Data
+{
+    public struct ModelMerger<T> where T : IDataModel
+    {
+        private static Func<T, bool> keyComparer;
+        private static Func<T, bool> valueComparer; 
+
+        static ModelMerger()
+        {
+
+        }
+
+        public ReadOnlyCollection<T> Same { get; private set; }
+
+        public ReadOnlyCollection<T> Insert { get; private set; }
+
+        public ReadOnlyCollection<T> Update { get; private set; }
+
+        public ReadOnlyCollection<T> Delete { get; private set; }
+
+        public ModelMerger(IEnumerable<T> oldModels, IEnumerable<T> newModels)
+        {
+            //Same = Key相同, Value相同
+            //Insert = Key不同, oldModel沒有且newModel有
+            //Delete = Key不同, oldModel有且newModel沒有
+            //Update = Key相同, Value不同
+
+            //new List<T>().AsReadOnly()
+        }
+
+    }
+}
